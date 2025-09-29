5896fa85ca6a39cd932bf64c0ae33234fb5ceffc
akinkeskinbas <keskinbas13@gmail.com>
2025-09-29 17:24:39 +0900

    fix: Add iOS camera launcher actual implementation
    - Add rememberCameraLauncher actual implementation for iOS
    - Prevent expect/actual declaration mismatch compile error
    - Add placeholder implementation with TODO for UIImagePickerController
    - iOS camera functionality to be implemented in future
    - Fixes red error indicator in IDE

--- composeApp/src/iosMain/kotlin/com/keak/petemotions/presentation/screens/CameraScreen.ios.kt
+++ composeApp/src/iosMain/kotlin/com/keak/petemotions/presentation/screens/CameraScreen.ios.kt
@@ -11,3 +11,14 @@ actual fun rememberGalleryLauncher(
         // TODO: Implement iOS photo picker
     }
 }
+
+@Composable
+actual fun rememberCameraLauncher(
+    onPhotoCaptured: (ByteArray) -> Unit
+): () -> Unit {
+    // For now, return empty lambda - needs UIImagePickerController implementation
+    return {
+        // TODO: Implement iOS camera capture
+        println("iOS: Camera capture not implemented yet")
+    }
+}
